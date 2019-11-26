1cafcdba6482e118c8b54050e57c92d405ffc1da
林万龙 <304115325@qq.com>
2019-11-26 15:13:44 +0800

    增加按钮事件

--- src/main/java/com/linwl/jFoenixDemo/controller/MainController.java
+++ src/main/java/com/linwl/jFoenixDemo/controller/MainController.java
@@ -1,6 +1,13 @@
 package com.linwl.jFoenixDemo.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @author ：linwl
@@ -8,6 +15,19 @@ import com.jfoenix.controls.JFXButton;
  * @description ：
  * @modified By：
  */
-public class MainController {
+public class MainController implements Initializable {
+    @FXML
     public JFXButton dialogButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void onStart(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("弹出的那个1");
+        alert.showAndWait();
+    }
 }
